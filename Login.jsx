8b70d84c1e01bfd2dd9ca7e70621acba281c445f
anil gurung <anilgrg4520@gmail.com>
2026-01-28 12:48:59 +0545

    first commit

---
+++ Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Form from './components/form/Form'
+const login = () => {
+  return (
+    <Form type='Login' />
+  )
+}
+
+export default login
---
+++ Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from './components/form/Form'
+
+const Register = () => {
+  return (
+    <Form type='Registe'/>
+  )
+}
+
+export default Register
